944e7d5355beb62dd89e19b2bd8251f42ee18dd7
Noam Bamberger <noamba@gmail.com>
2016-05-17 13:35:01 +0100

    Added a module to measure efficiency (timings) of comparable dataset searches.

--- efficiency_analysis.py
+++ efficiency_analysis.py
@@ -1,2 +1,33 @@
+#!/usr/bin/python3
+
 import timeit
+
+# Benchmark:
+
+number=1 # times the tested function is run for timing
+repeat=2 # test is repeated to get multiple results 
+
+
+print("**** Dataset as Dictionaries ****")
+setup = "import edited_better_ds as e"
+setup_with_loaded_dataset = setup + "; data_set = e.load_dataset('search_dataset.csv')"
+
+print("Time to load dataset: ")
+print(timeit.repeat("e.load_dataset('search_dataset.csv')", setup=setup, number=number, repeat=repeat))
+
+
+print("Time to process query file without printing output: ")
+print(timeit.repeat("e.process_query_file('queries.txt', False, *data_set)", setup=setup_with_loaded_dataset, number=number, repeat=repeat))
+
+
+print("\n\n**** Dataset as List ****")
+setup = "import edited_continued as e"
+setup_with_loaded_dataset = setup + "; data_set = e.load_dataset('search_dataset.csv')"
+
+print("Time to load dataset: ")
+print(timeit.repeat("e.load_dataset('search_dataset.csv')", setup=setup, number=number, repeat=repeat))
+
+
+print("Time to process query file without printing output: ")
+print(timeit.repeat("e.process_query_file('queries.txt', False, data_set)", setup=setup_with_loaded_dataset, number=number, repeat=repeat))
 
